8d6ff9dfa035979dcf166c1269cbe0867043697b
Daniele Ongari <danieleongari@Danieles-MacBook-Pro.local>
2018-10-09 22:52:29 +0200

    small mistake

--- make_gif/make_gif.sh
+++ make_gif/make_gif.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm *.png
-rm ../RENAME.gif
+rm ../README.gif
 
 gnuplot psisq_gif.gnuplot
 
@@ -10,4 +10,4 @@ do
 mv psisq_${i}.png psisq_0${i}.png
 done
 
-convert -delay 5 -loop 0 *.png ../REDAME.gif
+convert -delay 5 -loop 0 *.png ../README.gif
